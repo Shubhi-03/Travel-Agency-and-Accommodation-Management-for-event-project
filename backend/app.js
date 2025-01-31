7ff049b4460851af680cb15c132f2b0551a0e7f6
Shubhi-03 <mauryashubhi03@gmail.com>
2025-01-31 17:23:48 +0530

    fixed CORS

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,38 @@ import express, { application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 const app = express();
+const allowedOrigins = [process.env.ORIGIN, "http://localhost:3001"];
 
 app.use(cors({
-  origin: [process.env.ORIGIN, "http://localhost:3001"], 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true 
+  credentials: true
 }));
 
-app.options('*', cors());
+// Manually handle preflight requests
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
+
+// app.use(cors({
+//   origin: [process.env.ORIGIN, "http://localhost:3001"], 
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true 
+// }));
+
+// app.options('*', cors());
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
