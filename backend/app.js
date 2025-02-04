0ae680dbdba6142c6e977f8b7bf96c30d899aa21
Shubhi-03 <mauryashubhi03@gmail.com>
2025-02-04 19:46:34 +0530

    events dashboard added

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,15 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  console.log("CORS Headers Set For:", req.headers.origin);  // Logs the origin making the request
+  next();
+});
 app.options('*', cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
